c1bcd5d23c9c21b0160fdd2b97321e7d2831b786
Ivan <us.ivan05@gmail.com>
2022-12-03 00:37:24 +0800

    calendar

---
+++ MODER DEDLINES BOT/TEST.py
@@ -0,0 +1,113 @@
+import telebot
+from telebot import types
+from config import Token
+import datetime
+
+bot = telebot.TeleBot(Token)
+
+current_year = datetime.datetime.now().year
+current_month = datetime.datetime.now().month
+
+year = current_year
+month = current_month
+
+ru_month_array = ['Январь', 'Февраль', 'Март', 'Апрель',
+               'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь',
+               'Октябрь', 'Ноябрь', 'Декабрь']
+
+def make_calendar_keyboard(month = current_month, year = current_year):
+    keyboard = types.InlineKeyboardMarkup(row_width= 6)
+    quantity_of_days = int
+    callback_for_days = ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10',
+                             'd11', 'd12', 'd13', 'd14', 'd15', 'd16', 'd17', 'd18', 'd19',
+                             'd20', 'd21', 'd22', 'd23', 'd24', 'd25', 'd26', 'd27', 'd28',
+                             'd29', 'd30', 'd31']
+    days_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',
+                         '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25',
+                         '26', '27', '28', '29', '30', '31']
+    month_array = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+                   'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    edit_access = 0
+    month = month_array[month-1]
+    if month in ['jan','mar','may','jun','aug','oct','dec']:
+        quantity_of_days = 31
+    elif month == 'feb' and year%400 == 0: quantity_of_days = 29
+    elif month == 'feb' and year%100 == 0: quantity_of_days = 28
+    elif month == 'feb' and year%4 == 0: quantity_of_days = 29
+    elif month == 'feb': quantity_of_days = 28
+    else: quantity_of_days = 30
+
+    b1 = types.InlineKeyboardButton('<<год<<', callback_data='previous_year')
+    b2 = types.InlineKeyboardButton('>>год>>', callback_data='next_year')
+    b3 = types.InlineKeyboardButton('<<месяц<<', callback_data='previous_month')
+    b4 = types.InlineKeyboardButton('>>месяц>>', callback_data='next_month')
+
+    keyboard.add(b1, b2)
+    keyboard.add(b3, b4)
+
+    current = 0
+    DBR = []
+
+
+    for k in range(quantity_of_days // 6):
+        for k in range(6):
+            DBR += [types.InlineKeyboardButton(days_array[current], callback_data=callback_for_days[current])]
+            current+=1
+        keyboard.add(DBR[0], DBR[1], DBR[2], DBR[3], DBR[4], DBR[5])
+        DBR = []
+
+    for k in range(quantity_of_days % 6):
+        DBR += [types.InlineKeyboardButton(days_array[current], callback_data=callback_for_days[current])]
+        current += 1
+
+    if quantity_of_days % 6 == 1: keyboard.add(DBR[0])
+    if quantity_of_days % 6 == 4: keyboard.add(DBR[0], DBR[1], DBR[2], DBR[3])
+    if quantity_of_days % 6 == 5: keyboard.add(DBR[0], DBR[1], DBR[2], DBR[3], DBR[4])
+
+    #keyboard.add(types.InlineKeyboardButton("<<", callback_data="menu"))
+
+    return keyboard
+
+def main():
+
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        bot.send_message(message.chat.id, "<b>МЕНЮ</b>\n\nКоманды: /calendar", parse_mode='html')
+
+    @bot.message_handler(commands=['calendar'])
+    def calendar(message):
+        global year, month
+        year = current_year, month = current_month
+        keyboard = make_calendar_keyboard()
+        bot.send_message(message.chat.id, f"Год: {year}\nМесяц: {ru_month_array[month - 1]}", parse_mode='html', reply_markup=keyboard)
+
+
+    @bot.callback_query_handler(func = lambda call:True)
+    def edit(call):
+        global year, month
+        if call.data in ['previous_year', 'next_year', 'previous_month', 'next_month']:
+
+            if call.data == 'previous_year':
+                year-=1
+            if call.data == 'next_year':
+                year+=1
+            if call.data == 'previous_month':
+                month-=1
+                if month == 0:
+                    month = 12
+            if call.data == 'next_month':
+                month+=1
+                if month == 13:
+                    month = 1
+
+            bot.edit_message_text(f"Год: {year}\nМесяц: {ru_month_array[month - 1]}",chat_id = call.from_user.id , message_id = call.message.message_id)
+            keyboard = make_calendar_keyboard(month, year)
+            bot.edit_message_reply_markup(chat_id = call.from_user.id , message_id = call.message.message_id, reply_markup=keyboard)
+
+            if call.data == "menu":
+                start()
+
+if __name__ == "__main__":
+    main()
+
+bot.polling(long_polling_timeout=30)
---
+++ MODER DEDLINES BOT/functions.py
@@ -0,0 +1,87 @@
+import sqlite3
+
+class DeadDb:
+    """Класс для работы с базой данных бота"""
+
+    def __init__(self, data_base):
+        """подключение базы данных"""
+        self.connect = sqlite3.connect(data_base, check_same_thread=False)
+        self.cursor = self.connect.cursor()
+
+    def make_deadline(self, object_type, deadline_type, deadline_date):
+        """ввод в базу данных нового дедлайна"""
+        self.cursor.execute("INSERT INTO deadline ('object_type', 'deadline_type', 'deadline_date')"
+                            " VALUES (?, ?, ?)",
+                            (object_type, deadline_type, deadline_date))
+        return self.connect.commit()
+
+    def show_deadline(self, object_type, deadline_type):
+        """вывод в сообщении ближайшего дедлайна"""
+        self.cursor.execute("SELECT deadline_date"
+                            " FROM deadline "
+                            "WHERE object_type = ? AND deadline_type = ?"
+                            "ORDER BY deadline_date",
+                            (object_type, deadline_type))
+        a = str(self.cursor.fetchone())[2:-3]       #yyyy-mm-dd -> dd.mm.yyyy
+        #print(a)   #DELETE ON RELEASE
+        if a == '': return 'Дедлайнов нет'
+        date = a[8]+a[9]+'.'+a[5]+a[6]+'.'+a[0]+a[1]+a[2]+a[3]
+        return date
+
+    def object(self, object_type):
+        """вывод названия предмета"""
+        get = self.cursor.execute("SELECT object_name FROM object WHERE object_type = ?", (object_type,))
+        return str(self.cursor.fetchone())[2:-3]
+
+    def show_n_deadline(self, n):
+        """Показ n-го дедлайна"""
+        object_name = ''
+        type=''
+        date = ''
+        selection = ''
+        self.cursor.execute("SELECT object_type FROM deadline ORDER BY deadline_date")
+        selection = self.cursor.fetchall()
+        if len(selection) < n : return "Тут пусто"
+        object_name = selection[n-1]
+        self.cursor.execute("SELECT object_name FROM object WHERE object_type = ?", (str(object_name)[2:-3],))
+        object_name = str(self.cursor.fetchone())[2:-3]
+        #print(object_name, end='\t')        #DELETE ON RELEASE
+        self.cursor.execute("SELECT deadline_type FROM deadline WHERE rowid = ? ORDER BY deadline_date", (n,))
+        type = 'Д/З' if str(self.cursor.fetchone())[1:-2] == '0' else 'Л/Р'
+        #print(type, end ='\t')      #DELETE ON RELEASE
+        self.cursor.execute("SELECT deadline_date FROM deadline ORDER BY deadline_date")
+        a = str(self.cursor.fetchall()[n-1])[2:-3] #yyyy-mm-dd   ->   dd.mm.yyyy
+        #print(a)        #DELETE ON RELEASE
+        date = a[8]+a[9]+'.'+a[5]+a[6]+'.'+a[0]+a[1]+a[2]+a[3]
+        return (f"{object_name} {type}: {date}")
+
+    def auto_delete_deadline(self,):
+        """Автоудаление дедлайна по прошествии даты"""
+        self.cursor.execute("DELETE FROM deadline WHERE deadline_date < date('now')")
+        return self.connect.commit()
+
+    def print_data_base(self):
+        """Метод для тестирования базы"""
+        self.cursor.execute("SELECT * FROM object")
+        print(self.cursor.fetchall())
+
+    def show_all_deadline(self, object_type):
+        all_deadline_string= '              гггг-мм-дд'
+        self.cursor.execute("SELECT deadline_type, deadline_date FROM deadline WHERE object_type = ? ORDER BY deadline_date", (object_type, ))
+        all = self.cursor.fetchall()
+        for n in range(len(all)):
+            indexed_all=str(all[n])
+            if indexed_all[1] == '0':
+                indexed_all = indexed_all.replace('0', 'Д/З', 1)
+            else:
+                indexed_all = indexed_all.replace('1', 'Л/Р', 1)
+            all_deadline_string+=f"\n{n+1}. {indexed_all[1:-1]}"
+        #print(all_deadline_string)             DELETE ON RELEASE
+        if all_deadline_string == '              гггг-мм-дд': return "\nТут нечего удалять"
+        return all_deadline_string
+
+    def delete_deadline(self, object_type,n):
+        self.cursor.execute("SELECT deadline_type, deadline_date FROM deadline WHERE object_type = ? ORDER BY deadline_date", (object_type, ))
+        deadline = self.cursor.fetchall()[n-1]
+        self.cursor.execute("DELETE FROM deadline WHERE object_type = ? AND deadline_type = ? AND deadline_date = ?", (object_type, deadline[0], deadline[1]))
+        return self.connect.commit()
